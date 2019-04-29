5fa41395b608a4fbafa71400d38a23e13e3248ba
Adam Sultanov <adam.sultanov@gmail.com>
2019-04-29 17:23:04 -0400

    added those artists, boss

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,40 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select do |ptg|
+      ptg.gallery == self
+    end
+  end
+
+  def artists
+    paintings.map do |ptg|
+      ptg.artist
+    end
+  end
+
+  def artists_names
+    artists.map do |artist|
+      artist.name
+    end
+  end
+
+  def most_expensive_painting
+    paintings.max_by do |ptg|
+      ptg.price
+    end
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,25 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def self.all
+    @@all
+  end
+
+  def initialize(title, price, artist, gallery)
+    @artist = artist
+    @gallery = gallery
     @title = title
     @price = price
+    @@all << self
+  end
+
+  def self.total_price
+    self.all.reduce(0) do |sum, ptg|
+      sum += ptg.price
+    end
   end
 
 end
