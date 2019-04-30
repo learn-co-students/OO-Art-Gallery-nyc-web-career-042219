ea95dcca6ad9db39444c2f59f2dded2d32b9355d
Jessica Lin <jessicaa.lin@gmail.com>
2019-04-29 23:33:54 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,35 @@
 class Gallery
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
   attr_reader :name, :city
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.gallery == self }
+  end
+
+  def artists
+    self.paintings.map { |painting| painting.artist }
+  end
+
+  def artist_names
+    self.artists.map { |artist| artist.name }
+  end
+
+  def most_expensive_painting
+    #sort all paintings at the gallery by price
+    #take the highest/last index
+    self.paintings.sort { |painting| painting.price }[-1]
   end
 
 end
