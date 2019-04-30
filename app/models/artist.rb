421a5e7f2b3a8ce8a11b430c0b515bf668e7a94b
Jennifer Oh <ohjennifer.90@gmail.com>
2019-04-30 00:21:21 -0400

    oo-art-gallery

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,53 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    self.paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    self.galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  def self.total_experience
+    self.all.map do |artist|
+      artist.years_experience
+    end.sum
+  end
+
+  def self.most_prolific
+    self.all.sort_by do |artist|
+      painting_number = artist.paintings.count
+      experience = artist.years_experience
+      painting_number/experience.to_f
+    end.last
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+
+  def self.all
+    @@all
   end
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,45 @@
 class Gallery
 
   attr_reader :name, :city
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def paintings
+  Painting.all.select do |painting|
+    painting.gallery == self
+    end
+  end
+
+  def artists
+    self.paintings.map do |painting|
+      painting.artist
+    end
+  end
+
+  def artist_names
+    self.artists.map do |painting|
+      painting.name
+    end.uniq
+  end
+
+  def most_expensive_painting
+    Painting.all.max_by do |painting|
+      painting.price
+    end
+  end
+
+  def invested
+  end
+
+
+
+  def self.all
+    @@all
   end
 
 end
