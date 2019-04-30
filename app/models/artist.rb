4bd076d1edca5f0159159a0217ae735b5b61ba98
Pamyk <pamyk.ch@gmail.com>
2019-04-30 15:28:20 -0400

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,52 @@
 class Artist
 
+    @@all = []
+
   attr_reader :name, :years_experience
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+# Instance methods
+    def paintings
+        Painting.all.select {|painting| painting.artist  == self }
+    end
+
+    def galleries
+        self.paintings.map {|paint| paint.gallery }
+    end
+
+    def cities
+        self.galleries.map {|gallery| gallery.city}
+    end
+
+    def create_painting(title, price, gallary)
+        new_painting = Painting.new(title, price, self, gallary)
+
+    end
+
+
+    def add_recipe_card(recipe, date, rating)
+            new_rc = RecipeCard.new(self, recipe, rating)
+            new_rc.date = date
+    end
+
+# Class methods
+
+  def self.all
+      @@all
+  end
+
+  def self.total_experience
+      Artist.all.collect { |artist| artist.years_experience}.sum
+  end
+
+  def self.most_prolific
+      Artist.all.max_by {|artist| artist.paintings.length.to_f/artist.years_experience.to_f}
+  end
+
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,46 @@
 class Gallery
 
+    @@all = []
+
   attr_reader :name, :city
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
+
+
+  # Instance methods
+  def paintings
+      Painting.all.select {|painting| painting.gallery  == self }
+  end
+
+  def artists
+      Painting.all.select {|artist| artist.gallery  == self }
+  end
+
+  def artist_names
+     artists.map {|artist| artist.name}
+  end
+
+  def most_expensive_painting
+      price = 0
+      price_point = nil
+      Painting.all.each do |painting|
+          if painting.price > price
+              price = painting.price
+              price_point = painting
+          end
+      end
+          price_point
+      end
+
+  # Class methods
+  def self.all
+      @@all
+  end
+
+
 
 end
