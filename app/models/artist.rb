60fe53119af99fcffef8072edc267debb95c72a3
Jake <jakelovitz@gmail.com>
2019-04-30 09:23:19 -0400

    solved OO Art Gallery

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,42 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    self.paintings.collect { |painting| painting.gallery}
+  end
+
+  def cities
+    self.paintings.collect { |painting| painting.gallery.city}
+  end
+
+  def self.total_experience
+    Artist.all.collect { |artist| artist.years_experience}.sum
+  end
+
+  def self.most_prolific
+    Artist.all.max_by { |artist| artist.paintings.length.to_f / artist.years_experience.to_f }
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,17 @@
 require_relative '../config/environment.rb'
 
+#ARTISTS
+squidward = Artist.new("Squidward Tentacles", 2)
+vangogh = Artist.new("Vincent Van Gogh", 3)
 
+#Galleries
+met = Gallery.new("The Met", "NYC")
+louvre = Gallery.new("The Louvre", "Paris")
+
+#PAINTINGS
+bab = Painting.new("Bold and Brash", 500, squidward, met)
+genius = Painting.new("Misunderstood Genius", 200, squidward, met)
+starry = Painting.new("Starry night", 100, vangogh, louvre)
 
 
 binding.pry
