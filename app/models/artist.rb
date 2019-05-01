a5b9b442b157750a05ca299ac12797d6719b5208
Jason Gomez <jason.gomez@me.com>
2019-04-30 22:47:43 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,40 @@
 class Artist
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    all.map(&:years_experience).sum
+  end
+
+  def self.most_prolific
+    all.max_by { |artist| artist.paintings.length / artist.years_experience.to_f }
+  end
 
   attr_reader :name, :years_experience
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def galleries
+    paintings.map(&:gallery)
+  end
+
+  def cities
+    galleries.map(&:city)
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
